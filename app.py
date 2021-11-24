ef755682b1cd958f57b8017b01f70f9b52d69910
Maksym Shvets <maxshvets1901@gmail.com>
2021-11-24 19:21:20 +0200

    Implementing an endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -18,27 +18,48 @@ stores = [
 
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'Store not found'})
 
 
 @app.route('/store')
-def get_stores(name):
-    pass
+def get_stores():
+    return jsonify({'stores': stores})
 
 
 @app.route('/store', methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': [],
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 @app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store():
-    pass
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price'],
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'Store not found'})
 
 
 @app.route('/store/<string:name>/item')
-def get_items_in_store():
-    pass
+def get_items_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({store['items']})
+    return jsonify({'message': 'Store not found'})
 
 
 app.run(port=5000)
